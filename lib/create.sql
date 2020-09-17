3921dcc67aacb112e8fb29ab78eb6542c25fa5f0
Serhiy Bardysh <sbardysh@tutanota.com>
2020-09-17 03:00:46 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,17 @@
 CREATE TABLE bears (
+<<<<<<< HEAD
   id INTEGER PRIMARY KEY,
+=======
+  id INTEGER,
+>>>>>>> 2f0095c68146542155c7d544e37a81f197055901
   name TEXT,
   age INTEGER,
   gender TEXT, 
   color TEXT,
+<<<<<<< HEAD
   temperament TEXT,
+=======
+  temperature TEXT,
+>>>>>>> 2f0095c68146542155c7d544e37a81f197055901
   alive NUMERIC
 );
